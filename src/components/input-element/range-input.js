fe684b890d6c904d34f5d5c861b55304fd4c118a
Chuck Stephens <chuckstephens2076@gmail.com>
2023-12-11 14:33:47 -0600

    range slider working

---
+++ src/components/input-element/range-input.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const RangeSlider = (props) => {
+
+    return (
+        <>
+            <div className="range-slider">
+                <input
+                    type="range"
+                    min={props.min}
+                    max={props.max}
+                    value={props.value}
+                    step={props.step}
+                    {...props} />
+                <span>{props.value}</span>
+            </div>
+        </>
+    )
+}
+
+export default RangeSlider
